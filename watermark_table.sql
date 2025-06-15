a41ded06ef650647f02c4308be8ea740f7561584
Shivaram Babar <shivaramb1702@gmail.com>
2025-06-15 18:11:35 +0530

    updated watermark table

--- watermark_table.sql
+++ watermark_table.sql
@@ -7,17 +7,17 @@ CREATE TABLE water_table
 SELECT * FROM water_table;
 
 
-SELECT min(Date_ID) FROM [dbo].[source_cars_data]
+SELECT min(Date_ID) FROM [dbo].[source_car_data]
 
 
 INSERT INTO water_table 
 VALUES('DT00000')
 
 
-SELECT * FROM [dbo].[source_cars_data] where Date_ID > 'DT00000'
+SELECT * FROM [dbo].[source_car_data] where Date_ID > 'DT00000'
 
 -- Compare this snippet from stored_procedure_update_watermark.sql:
-SELECT count(*) FROM [dbo].[source_cars_data] where Date_ID > 'DT00000'
+SELECT count(*) FROM [dbo].[source_car_data] where Date_ID > 'DT00000'
 
-SELECT count(*) FROM [dbo].[source_cars_data]
+SELECT count(*) FROM [dbo].[source_car_data]
 
